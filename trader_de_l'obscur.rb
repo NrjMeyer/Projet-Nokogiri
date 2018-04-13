04ae735c096fa0bf03a9f9d8e0f37184c4d89f08
NrjMeyer <37902950+NrjMeyer@users.noreply.github.com>
2018-04-13 12:19:05 +0300

    First Commit

--- trader_de_l'obscur.rb
+++ trader_de_l'obscur.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 
 def cours_cryptomonnnaies
     page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    tab_cours_actuel = []
     cours_actuel = Hash.new(0)
     puts "*********************************************************************"
     puts "                       CHARGEMENT                                    "
@@ -12,19 +13,19 @@ def cours_cryptomonnnaies
     element_tab = page.css("#currencies-all tbody tr")[0..12]
     for i in 0...11 do
         x = page.css("#currencies-all tbody tr")[i].text.split("\n").join(" ")[2..-1].split(" ")
-        cours_actuel.push({
-            :name => x[2],
-            :symbol => x[3],
-            :market_cap => x[4],
-            :price => x[5],
-            :circulating_supply => x[6],
-            :volume => x[7],
-            :percent_1_h => x[8],
-            :percent_24_h => x[9],
-            :percent_7_d =>x[10]
+        cours_actuel[:name] = x[2]
+        cours_actuel[:symbol] = x[3]
+        cours_actuel[:market_cap] = x[4]
+        cours_actuel[:price] = x[5]
+        cours_actuel[:circulating_suply] = x[6]
+        cours_actuel[:volume] = x[7]
+        cours_actuel[:percent_1_h] = x[8]
+        cours_actuel[:percent_24_h] = x[9]
+        cours_actuel[:percent_7_d] = x[10]
+        tab_cours_actuel << cours_actuel    
         })
     end
-    cours_actuel
+    tab_cours_actuel
 end
 
 #Boucle infini qui se lance chaque une heure 
